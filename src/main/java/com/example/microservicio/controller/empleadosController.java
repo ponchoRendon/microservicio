2348bb33b2d44b482a39c798ad70b4a60e206178
poncho <alfonso.rendonb@hotmail.com>
2020-10-05 11:06:18 -0500

    para invalidar cors origin

--- src/main/java/com/example/microservicio/controller/empleadosController.java
+++ src/main/java/com/example/microservicio/controller/empleadosController.java
@@ -14,6 +14,7 @@ import java.net.URISyntaxException;
 import java.util.List;
 
 @RefreshScope
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST})
 @RestController
 @RequestMapping("/api/v1/empleados")
 public class empleadosController {
